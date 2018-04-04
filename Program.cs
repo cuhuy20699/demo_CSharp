fb3906688705717ab6caf1cc656f29344d4b6f03
Ngoc Huy <cuhuy20699@gmail.com>
2018-04-04 09:13:54 +0700

    news

---
+++ Program.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using MySql.Data.MySqlClient;
+
+namespace FormQL_SinhVien
+{
+
+    class Program
+    {
+        private MySqlConnection connection;
+        private string server;
+        private string database;
+        private string uid;
+        private string password;
+
+        public Program()
+        {
+            initilizer();
+        }
+
+        private void initilizer()
+        {
+            server = "localhost";
+            database = "quanlysv";
+            uid = "root";
+            password = "";
+            string connectionString;
+            connectionString = "SERVER=" + server + ";" + "DATABASE=" +
+            database + ";" + "UID=" + uid + ";" + "PASSWORD=" + password + ";CharSet=utf8;";
+
+            connection = new MySqlConnection(connectionString);
+        }
+        private bool OpenConnection()
+        {
+            try
+            {
+                connection.Open();
+                return true;
+            }
+            catch (MySqlException ex)
+            {
+                //When handling errors, you can your application's response based 
+                //on the error number.
+                //The two most common error numbers when connecting are as follows:
+                //0: Cannot connect to server.
+                //1045: Invalid user name and/or password.
+                switch (ex.Number)
+                {
+                    case 0:
+                        Console.WriteLine("Error");
+                        break;
+
+                    case 1045:
+                        Console.WriteLine("Error");
+                        break;
+                }
+                return false;
+            }
+        }
+
+        //Close connection
+        private bool CloseConnection()
+        {
+            try
+            {
+                connection.Close();
+                return true;
+            }
+            catch (MySqlException)
+            {
+                Console.WriteLine("Error");
+                return false;
+            }
+        }
+
+        public void Insert(Student student)
+        {
+            if (student == null)
+            {
+                throw new ArgumentNullException(nameof(student));
+            }
+
+            string query = "INSERT INTO student (full_name, roll_number) VALUES('"+ student.Name + "' , '"+ student.Rollnumber+ "')";
+
+            //open connection
+            if (this.OpenConnection() == true)
+            {
+                //create command and assign the query and connection from the constructor
+                MySqlCommand cmd = new MySqlCommand(query, connection);
+
+                //Execute command
+                cmd.ExecuteNonQuery();
+
+                //close connection
+                this.CloseConnection();
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            Program p = new Program();
+            Student student = new Student("abc","12231");
+            p.Insert(student);
+        }
+
+       
+    }
+}
---
+++ Student.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FormQL_SinhVien
+{
+    class Student
+    {
+        private string name;
+        private string rollnumber;
+
+        public Student()
+        {
+            
+        }
+
+        public Student(string name, string rollnumber)
+        {
+            this.name = name;
+            this.rollnumber = rollnumber;
+        }
+
+        public string Name { get => name; set => name = value; }
+        public string Rollnumber { get => rollnumber; set => rollnumber = value; }
+    }
+}
